ea5803f25f3076a5b7e032c257275a511443884e
Chandu <chanduvallepu630@gmail.com>
2026-07-27 13:04:31 +0530

    Add smooth scroll and typing animation features

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log('Portfolio loaded');
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+anchor.addEventListener('click',function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+behavior:'smooth'
+});
+});
+});
+
+// Typing animation
+const words=[
+"Software Developer",
+"Java Developer",
+"Python Developer",
+"Web Developer",
+"AI Enthusiast"
+];
+
+let i=0;
+let j=0;
+let current="";
+let isDeleting=false;
+
+const typing=document.querySelector(".typing");
+
+function type(){
+
+current=words[i];
+
+if(!isDeleting){
+typing.textContent=current.substring(0,j++);
+if(j>current.length){
+isDeleting=true;
+setTimeout(type,1500);
+return;
+}
+}else{
+typing.textContent=current.substring(0,j--);
+if(j<0){
+isDeleting=false;
+i=(i+1)%words.length;
+}
+}
+
+setTimeout(type,isDeleting?60:120);
+}
+
+if(typing){
+type();
+}
